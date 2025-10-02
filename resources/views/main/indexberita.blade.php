8f3631af0b868e351755029b54a47898c80bb8b9
Noctissinnit <ncts.yoru@gmail.com>
2025-10-02 12:13:20 +0700

    setup main

--- resources/views/main/indexberita.blade.php
+++ resources/views/main/indexberita.blade.php
@@ -1,21 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Berita UPITRA Software Engineering</title>
-    <link rel="stylesheet" href="resources/css/mainberita.css">
-</head>
-<body>
-    <table>
-        <thead>
-                <th>Nomor</th>
-                <th>Judul Acara</th>
-                <th>Tanggal</th>
-                <th>Penulis</th>
-                <th>Aksi</th>
-        </thead>
-    </table>
-</body>
-</html>
+@extends('layouts.app')
+@section('content')
+  
+        <h1>Ini berita</h1>
+        <table>
+            <thead>
+                    <th>Nomor</th>
+                    <th>Judul Acara</th>
+                    <th>Tanggal</th>
+                    <th>Penulis</th>
+                    <th>Aksi</th>
+            </thead>
+        </table>
+
+@endsection
