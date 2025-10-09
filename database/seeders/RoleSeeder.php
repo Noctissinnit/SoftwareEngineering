5ea7038001448e4b05650a16db4840579328e69f
Noctissinnit <ncts.yoru@gmail.com>
2025-10-09 11:30:31 +0700

    Add admin user

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -11,12 +11,10 @@ class RoleSeeder extends Seeder
 {
     public function run()
     {
-     
+        // Buat role admin jika belum ada
         $adminRole = Role::firstOrCreate(['name' => 'admin']);
 
-        $userRole = Role::firstOrCreate(['name' => 'user']);
-
-     
+        // Buat Super Admin
         $admin = User::firstOrCreate(
             ['email' => 'admin@example.com'],
             [
@@ -24,7 +22,36 @@ class RoleSeeder extends Seeder
                 'password' => Hash::make('password'),
             ]
         );
-
         $admin->assignRole($adminRole);
+
+        // Buat user Pieter
+        $pieter = User::firstOrCreate(
+            ['email' => 'pieter@example.com'],
+            [
+                'name' => 'Pieter',
+                'password' => Hash::make('password'),
+            ]
+        );
+        $pieter->assignRole($adminRole);
+
+        // Buat user Nikita
+        $nikita = User::firstOrCreate(
+            ['email' => 'nikita@example.com'],
+            [
+                'name' => 'Nikita',
+                'password' => Hash::make('password'),
+            ]
+        );
+        $nikita->assignRole($adminRole);
+
+        // Buat user Arthur
+        $arthur = User::firstOrCreate(
+            ['email' => 'arthur@example.com'],
+            [
+                'name' => 'Arthur',
+                'password' => Hash::make('password'),
+            ]
+        );
+        $arthur->assignRole($adminRole);
     }
 }
