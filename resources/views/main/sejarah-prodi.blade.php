bba2ef5a3b2e5285824fd09f5a18f5c1473428cb
nktaaxyz <chandranikita370@gmail.com>
2025-11-13 11:09:00 +0700

    sejarah-prodi

--- resources/views/main/sejarah-prodi.blade.php
+++ resources/views/main/sejarah-prodi.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.main')
 
 @section('content')
-     <h1 class="mb-4" style="color: #03378c;">Sejarah Prodi</h1>
+     <div class="text-center mb-5">
+        <h1 class="fw-bold text-primary">Tujuan Prodi</h1>
+        <p class="text-muted">Program Studi Software Engineering - Universitas Pignatelli Triputra</p>
+    </div>
+    <section class="mb-3 py-4 bg-light">
+        <ul>
+            <li>Berkontribusi dalam memperluas akses pendidikan tinggi yang berkualitas dan terjangkau bagi masyarakat di bidang rekayasa perangkat lunak.</li>
+            <li>Menghasilkan sarjana bidang Rekayasa Perangkat Lunak yang bermoral, berintegritas, profesional, bertanggung jawab, dan mampu berkarya dengan keahliannya di bidang rekayasa perangkat lunak.</li>
+            <li>Berkontribusi dalam pengembangan dan penelitian perangkat lunak yang unggul, solutif, inovatif dan transformatif bagi masyarakat dan kehidupan.</li>
+            <li>Menerapkan ilmu rekayasa perangkat lunak yang berdaya guna dan berhasil guna bagi masyarakat.</li>
+            <li>Menjalin kerja sama dengan dunia pendidikan, masyarakat, pemerintah dan industri yang berkelanjutan, beretika, dan bermanfaat di bidang rekayasa perangkat lunak.</li>
+        </ul>
+    </section>
 @endsection
--- resources/views/main/visi-misi.blade.php
+++ resources/views/main/visi-misi.blade.php
@@ -6,10 +6,10 @@
         <p class="text-muted">Program Studi Software Engineering - Universitas Pignatelli Triputra</p>
     </div>
        
-     <section class="mb-4">
-        <h2 class="text-primary">Visi</h2>
+     <section class="mb-3 py-4 bg-light">
+        <h2 class="text-center text-primary">Visi</h2>
         <p>Program studi yang unggul dalam bidang rekayasa perangkat lunak, berorientasi global, menjunjung tinggi nilai-nilai integritas dan bersemangat kebhinekaan.</p>
-           <h2 class="text-primary">Misi</h2>
+           <h2 class="text-center text-primary">Misi</h2>
         <ul>
             <li>Menyelenggarakan program studi Rekayasa Perangkat Lunak secara efektif dan efisien untuk mendukung terlaksananya Tri Dharma perguruan tinggi.</li>
             <li>Menghasilkan sarjana di bidang rekayasa perangkat lunak yang kompeten, solutif, berpola pikir logis dan sistematis, memiliki kedalaman spiritual, menjunjung kemanusiaan, rendah hati, berintegritas dan profesional dalam memanfaatkan ilmu rekayasa perangkat lunak di lingkungan kerja maupun kehidupan bermasyarakat.</li>
